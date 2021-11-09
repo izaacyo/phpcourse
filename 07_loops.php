90e5264539430aaab03e730185b0d3f31c2b42ca
izaacyo <mariusppetrut@gmail.com>
2021-11-09 23:31:13 +0200

    loops

--- 07_loops.php
+++ 07_loops.php
@@ -4,10 +4,55 @@
 
 // Loop with $counter
 
+$counter = 0;
+while ($counter < 10) {
+    echo $counter . '<br>';
+    if ($counter === 5) {
+        break;
+    }
+    $counter++;
+}
+
 // do - while
+// executes it once while above it will never execute if counter = 0
+
+do {
+    echo $counter;
+    $counter++;
+} while ($counter < 10);
+
 
 // for
 
+for ($i = 0; $i < 10; $i++) {
+    echo $i . '<br>';
+}
+
+
 // foreach
 
+$fruits = ["Banana", "Apple", "Orange"];
+
+foreach ($fruits as $i => $fruit) {
+    echo $i . ' ' . $fruit . '<br>';
+}
+
+
 // Iterate Over associative array.
+
+
+$person = [
+    'name' => 'Brad',
+    'surname' => 'Traversy',
+    'age' => 30,
+    'hobbies' => ['Tennis', 'Video Games']
+];
+
+
+foreach ($person as $key => $value) {
+    if (is_array($value)) {
+        echo $key . ' ' . implode(" , ", $value) . '<br>';
+    } else {
+        echo $key . ' ' . $value . '<br>';
+    }
+};
