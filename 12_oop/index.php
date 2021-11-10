5258cc036cf40cd0de4316ea34c93d3cfb103e82
izaacyo <mariusppetrut@gmail.com>
2021-11-10 09:55:37 +0200

    object oriented

--- 12_oop/index.php
+++ 12_oop/index.php
@@ -1,6 +1,31 @@
 <?php
 
+require_once './Person.php';
+require_once './Student.php';
+
+
+$student = new Student("Marius", "Petrut", '1234');
+
 // What is class and instance
+
+$p = new Person('Brad', 'Traversy');
+
+$p->setAge(30);
+
+echo '<pre>';
+var_dump($p);
+echo '</pre>';
+
+echo $p->name . '<br>';
+echo $p->getAge();
+
+$p2 = new Person('John', 'Smith');
+echo '<pre>';
+var_dump($p2);
+echo '</pre>';
+echo Person::getCounter();
+
+
 
 // Create Person class in Person.php
 
