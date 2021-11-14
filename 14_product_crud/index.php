240ba46e746c5360eaedf69f3b660c311a28affe
izaacyo <mariusppetrut@gmail.com>
2021-11-14 17:08:30 +0200

    display db products

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -4,6 +4,16 @@ $pdo = new PDO('mysql:host=localhost; port=3306;dbname=products_crud', 'root', '
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+$statement->execute();
+
+//fetch as assosiative array
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+/*echo '<pre>';
+var_dump($products);
+echo '</pre>';*/
+
 ?>
 
 
@@ -30,29 +40,31 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Image</th>
+                <th scope="col">Title</th>
+                <th scope="col">Price</th>
+                <th scope="col">Create Date</th>
+                <th scope="col">Action</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
+            <?php foreach ($products as $i => $product) : ?>
+                <tr>
+                    <th scope="row"><?php echo $i + 1 ?> </th>
+                    <td> </td>
+                    <td><?php echo $product['title'] ?> </td>
+                    <td><?php echo $product['price'] ?> </td>
+                    <td><?php echo $product['create_date'] ?> </td>
+                    <td>
+                        <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
+                        <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
+
+                    </td>
+
+                </tr>
+
+            <?php endforeach; ?>
+
         </tbody>
     </table>
 </body>
